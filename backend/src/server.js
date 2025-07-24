c43c7527c9d2ba1c2ea9df7443fb7bc4bf2d0993
Alec <narq.99@gmail.com>
2025-07-24 16:21:06 +0800

    prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import noteRoutes from './routes/noteRoutes.js';
 import connectDB from './config/db.js';
@@ -10,18 +11,30 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 
 // Middleware
-app.use(cors({
-    origin: "http://localhost:5173", // Adjust this to your frontend URL
-    credentials: true, // Allow credentials if needed
-}))
+if(process.env.NODE_ENV === 'production') {
+    app.use(cors({
+        origin: process.env.FRONTEND_URL || 'http://localhost:3000'
+    }));
+}
 app.use(express.json()); // this middleware parses incoming JSON requests: req.body
 app.use(rateLimiter);
 
 // Routes
 app.use("/api/notes", noteRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    // Serve static files from the React frontend app
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+// Set the base URL for the API
+// Use environment variable for production or default to local development
+const BASE_URL = import.meta.env.VITE_API_BASE_URL ? 'http://localhost:5001/api':"/api";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: BASE_URL,
   timeout: 15000, // 15 seconds
   headers: {
     'Content-Type': 'application/json',
